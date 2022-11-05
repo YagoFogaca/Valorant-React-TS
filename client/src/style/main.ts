fbcb9f810bd07b89915257d190e0dede4463c603
Yago Fogaca <yagofogaca20@gmail.com>
2022-11-05 11:46:18 -0300

    feat: add ubuntu font

--- client/src/style/main.ts
+++ client/src/style/main.ts
@@ -1,16 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-  font-size: 10px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    font-size: 10px;
+    font-family: 'Ubuntu', sans-serif;
+  }
 
-html, body, #root {
+  html,
+  body {
     display: flex;
     flex-direction: column;
     width: 100%;
     height: 100%;
     background-color: #3d3c3c;
-}`;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    background-color: #3d3c3c;
+  }
+`;
